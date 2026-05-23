4c5c7c1c4509855028d018384abf10d212c67682
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-23 02:52:11 +0200

    feat: add src/facebook/index.ts

---
+++ src/facebook/index.ts
@@ -0,0 +1,14 @@
+export { FacebookConnection, ConnectionState } from "./FacebookConnection";
+export { FacebookClient } from "./FacebookClient";
+export { FacebookSender } from "./FacebookSender";
+export { FacebookEventNormalizer } from "./FacebookEventNormalizer";
+export { FacebookGateway } from "./FacebookGateway";
+export type { MessageHandler } from "./FacebookGateway";
+export type { ISender } from "./types/ISender";
+export type {
+  FBEvent,
+  FBMessageEvent,
+  FBPostbackEvent,
+  FBUnknownEvent,
+  FBAttachment,
+} from "./types/events";
