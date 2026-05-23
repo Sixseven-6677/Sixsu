55773dbfc9a3a268128a9858c5a47e1e2d19adef
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-23 03:02:06 +0200

    feat: add src/commands/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,6 @@ export { CommandRegistry } from "./CommandRegistry";
 export { CommandLoader } from "./CommandLoader";
 export { CommandPipeline } from "./CommandPipeline";
 export type { NotFoundHandler } from "./CommandPipeline";
-export type { ICommand, PipelineMiddleware } from "./types/ICommand";
+export type { ICommand } from "./types/ICommand";
 export { loggingMiddleware } from "./middleware/logging.middleware";
 export { typingMiddleware } from "./middleware/typing.middleware";
